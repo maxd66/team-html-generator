a08658a686d259e256ae7f8165b481df139bb581
maxd66 <max.dunn63@gmail.com>
2021-07-12 23:46:50 -0700

    added test for getName, passing test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,15 @@
 // const { it } = require('@jest/globals');
 // const { describe } = require('yargs');
+const { it, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
     describe('constructor', () => {
         it('should return a constructor with name, id, and email properties', () => {
             // setup
-            name = 'Jerry';
-            id = 117;
-            email = 'sony@playstation.com';
+            const name = 'Jerry';
+            const id = 117;
+            const email = 'sony@playstation.com';
             const expected = {
                 name: 'Jerry',
                 id: 117,
@@ -18,6 +19,13 @@ describe('Employee', () => {
             const employee = new Employee(name, id, email);
             // expectation
             expect(employee).toMatchObject(expected);
+        })
+        it('when getName is called, method returns name of associated oject', () => {
+            const employee = new Employee('Rick', 25, 'sony@playstation.com')
+
+            const empName = employee.getName();
+
+            expect(empName).toEqual('Rick');  
         })
     })
 })
