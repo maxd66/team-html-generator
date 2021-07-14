61ea37b86cda492c00761b40c1c0110d4ae61bd0
maxd66 <max.dunn63@gmail.com>
2021-07-13 17:12:04 -0700

    added tests for getRole, and getSchool to intern class, both now passing

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,12 +1,24 @@
-const { describe, expect } = require('@jest/globals');
+const { describe, expect, it } = require('@jest/globals');
 const Intern = require('../lib/Intern');
 
 describe('Intern', () => {
     describe('constructor', () => {
         it('should return an object with an additional school property', () => {
-            const intern = new Intern('Sarah', 13, 'neverForgotten@bestDaughter.com', 'kiddo university')
+            const intern = new Intern('Sarah', 13, 'neverForgotten@bestDaughter.com', 'kiddo university');
 
-            expect(intern).toHaveProperty('school', 'kiddo university')
+            expect(intern).toHaveProperty('school', 'kiddo university');
+        })
+    })
+    describe('methods', () => {
+        it('should return name of school associated with object when getSchool is called', () => {
+            const intern = new Intern('TARS', 365, 'funniestRobot@NASA.com', 'USMC Academy');
+
+            expect(intern.getSchool()).toEqual('USMC Academy');
+        })
+        it('should return string intern when get role is called on intern object', () => {
+            const intern = new Intern('CASE', 366, 'TARS_talksEnoughForBothOfUs@NASA.com', 'USMC Academy');
+
+            expect(intern.getRole()).toEqual('Intern');
         })
     })
 })
